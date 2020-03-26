e44abe8c7b0fda23977ce54d4a357de84bfa00c9
Szhangsir <zhangfq88@gmail.com>
2020-03-26 20:24:45 +0800

    first commit

---
+++ ex3.py
@@ -0,0 +1,7 @@
+print ("I will now count my chickens:")
+
+#计算母鸡的价格
+print ('Hens',25+30/6)
+print ("Roosters",100-25*3%4)
+
+print ("Now I will count the eggs:")
---
+++ ex5.py
@@ -0,0 +1,9 @@
+name='Shuo Zhang'
+age=21#not a lie
+height=71 #inches
+eyes='Block'
+teeth='White'
+
+print ("Let's talk about %s." % name)
+
+print ("my height is %d inches, %.2f cm." % (height, height*2.54))
